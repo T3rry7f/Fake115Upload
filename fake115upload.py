68fe7993baf924cd3a659dd79c642a77c9f572dc
T3rry7f <t3rry7f@gmail.com>
2020-11-26 18:37:09 +0800

    m115 encrytion support

--- fake115upload.py
+++ fake115upload.py
@@ -5,27 +5,37 @@ __author__ = 'T3rry'
 from requests_toolbelt.multipart.encoder import MultipartEncoder 
 from ecdsa import ECDH, NIST224p,SigningKey
 from Crypto.Cipher import AES
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from Crypto import Random
 import hashlib,base64,lz4.block,zlib
 import sys,os,time,platform
 import json,requests,random
 import getopt,ctypes,codecs
-
+import urllib
+import numpy as np
 
 COOKIE='need your cookie'
 
 class Fake115Client(object):
 
 	def __init__(self, cookie):
-		self.version='23.9.1'
+		self.version='23.9.0'
 		self.app_version='25.2.2'
 		self.cookie=cookie
-		self.ua='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36 115Browser/23.9.1'
+		self.ua='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36 115Browser/23.9.0'
 		self.content_type='application/x-www-form-urlencoded'
 		self.header={"User-Agent" : self.ua,"Content-Type": self.content_type, "Cookie":self.cookie }
 		self.remote_pubkey='0457A29257CD2320E5D6D143322FA4BB8A3CF9D3CC623EF5EDAC62B7678A89C91A83BA800D6129F522D034C895DD2465243ADDC250953BEEBA'
 		self.crc_salt='^j>WD3Kr?J2gLFjD4W2y@'
+		self.private_key = RSA.construct((0x8C81424BC166F4918756E9F7B22EFAA03479B081E61896872CB7C51C910D7EC1A4CE2871424D5C9149BD5E08A25959A19AD3C981E6512EFDAB2BB8DA3F1E315C294BD117A9FB9D8CE8E633B4962E087C629DC6CA3A149214B4091EF2B0363CB3AE6C7EE702377F055ED3CD93F6C342256A76554BBEA7F203437BBE65F2DA2741, 0x10001, 0x3704DAB00D80C25E464FFB785A16D95F688D0A5823811758C16308D5A1DB55FA800D967A9B4AEDE79AA783ADFFDCDB23541C80B8D436901F172B1CCCA190B224DBE777BF18B96DD9A30AACE8780350793A4F90A645A7747EF695622EADBE23A4C6D88F22E87842B43B35486C2D1B5B1FA77DB3528B0910CA84EDB7A46AFDBED1))
+		self.publickey = RSA.construct((0xD1DEB5A67989D10A5C0B041D11AB47659BC32B3E65A92F9236DD2CC2140A0DA495FA0E7EF875F6FACB4D981F5E5E3BA5E67811DD73B7A4105254222FACC206A74E8911215451AFFCF5EBFDE887BC9ADB675C02A835F75CBE77A603AF820683F439E7CED4289323DF9C9055769E15798B9453E89058B3E2E3F186B07B046A996D,0x10001))
+		self.g_key_l='\x42\xda\x13\xba\x78\x76\x8d\x37\xe8\xee\x04\x91'
+		self.g_key_s='\x29\x23\x21\x5e'
+		self.g_kts='\xf0\xe5\x69\xae\xbf\xdc\xbf\x5a\x1a\x45\xe8\xbe\x7d\xa6\x73\x88\xde\x8f\xe7\xc4\x45\xda\x86\x94\x9b\x69\x92\x0b\x6a\xb8\xf1\x7a\x38\x06\x3c\x95\x26\x6d\x2c\x56\x00\x70\x56\x9c\x36\x38\x62\x76\x2f\x9b\x5f\x0f\xf2\xfe\xfd\x2d\x70\x9c\x86\x44\x8f\x3d\x14\x27\x71\x93\x8a\xe4\x0e\xc1\x48\xae\xdc\x34\x7f\xcf\xfe\xb2\x7f\xf6\x55\x9a\x46\xc8\xeb\x37\x77\xa4\xe0\x6b\x72\x93\x7e\x51\xcb\xf1\x37\xef\xad\x2a\xde\xee\xf9\xc9\x39\x6b\x32\xa1\xba\x35\xb1\xb8\xbe\xda\x78\x73\xf8\x20\xd5\x27\x04\x5a\x6f\xfd\x5e\x72\x39\xcf\x3b\x9c\x2b\x57\x5c\xf9\x7c\x4b\x7b\xd2\x12\x66\xcc\x77\x09\xa6'
+		self.m115_rnd_key=None
 		self.curve = NIST224p
-		self.local_private_key=None        
+		self.local_private_key=None		
 		self.aes_key=None
 		self.aes_iv=None
 		self.local_public_key=None
@@ -35,7 +45,7 @@ class Fake115Client(object):
 		self.filecount=0
 		self.cid=0
 		
-		sk =SigningKey.generate(curve=self.curve)         
+		sk =SigningKey.generate(curve=self.curve)		 
 		ecdh = ECDH(curve=self.curve)
 		ecdh.load_private_key(sk)
 		local_public_key = ecdh.get_public_key()._compressed_encode()
@@ -48,7 +58,68 @@ class Fake115Client(object):
 
 		if self.get_userkey() is False: 
 			print('Get userkey info failed!')
-	
+
+	def m115_xorinit(self,randkey,sk_len):
+  
+		length=sk_len *(sk_len-1)
+		index=0
+		xorkey=''
+		for i in range(0,sk_len):
+			x=np.uint8(ord(randkey[i]) + ord(self.g_kts[index]))	
+			xorkey += chr(ord(self.g_kts[length])^ x)
+			length -=sk_len
+			index +=sk_len
+
+		self.m115_rnd_key=randkey
+	   
+		if(sk_len==4):
+			self.g_key_s=xorkey
+		elif(sk_len==12):
+			self.g_key_l=xorkey
+
+	def xor115_enc(self,src,key):
+		lkey=len(key)
+		secret=[]
+		num=0
+		for s in src:
+			if num>=lkey:
+				num=num%lkey
+			secret.append( chr( ord(s)^ord(key[num]) ) )
+			num+=1
+		return "".join(secret)
+
+	def  m115_encode(self,plaintext):
+
+		tmp=self.xor115_enc(plaintext,self.g_key_s)[::-1]
+		xortext= self.xor115_enc(tmp,self.g_key_l)
+		cipher = PKCS1_v1_5.new(self.publickey)
+		ciphertext = cipher.encrypt(self.m115_rnd_key+xortext)
+		ciphertext= urllib.quote(base64.b64encode(ciphertext))
+
+		return ciphertext
+
+	def  m115_decode(self,ciphertext):
+
+		cipher = PKCS1_v1_5.new(self.private_key)
+		sentinel = Random.new().read(15+20)
+		key_size=16
+		block_size=128
+		plaintext=''
+		ciphertext=base64.b64decode(ciphertext)
+		block=len(ciphertext)/block_size
+
+		for i in range(0,block):
+			plaintext+=cipher.decrypt(ciphertext[i*128:block_size],sentinel)
+			block_size+=128
+
+		randkey= plaintext[0:key_size]
+		plaintext=plaintext[key_size:]
+		self.m115_xorinit(randkey,12)
+		tmp= self.xor115_enc(plaintext,self.g_key_l)[::-1]
+		plaintext= self.xor115_enc(tmp,self.g_key_s)
+
+		return plaintext
+
 	def ec115_get_token(self,data): #md5(fileid+filesize+preid+uid+timestap+md5(uid))
 		m = hashlib.md5()
 		m.update(data)
@@ -70,7 +141,6 @@ class Fake115Client(object):
 		tmp=''
 
 		cryptos = AES.new(self.aes_key, mode)
-		
 		for i in range(0,len(data)):
 			tmp+=chr(ord(data[i])^ord(xor_key[i%16]))
 			if((i%16)==15):	
@@ -102,10 +172,10 @@ class Fake115Client(object):
 			tmp+=chr(r2)+chr(1^r2)
 			for x in xrange(0,3):
 				tmp+=chr(r2)
-	        
+			
 			crc= zlib.crc32(self.crc_salt+tmp)& 0xffffffff
 			h_crc32= hex(crc)[2:]
-			if(len(h_crc32)%2 !=0):	        	
+			if(len(h_crc32)%2 !=0):				
 				h_crc32='0'+h_crc32
 
 			h_crc32=h_crc32.decode('hex')
@@ -127,11 +197,11 @@ class Fake115Client(object):
 		return self.ec115_compress_decode(lz4_buff)
 
 	def set_cmd_text_color(self,color):
-	    Bool = ctypes.windll.kernel32.SetConsoleTextAttribute(self.std_out_handle, color)
-	    return Bool
+		Bool = ctypes.windll.kernel32.SetConsoleTextAttribute(self.std_out_handle, color)
+		return Bool
 
 	def resetColor(self):
-	    self.set_cmd_text_color(0x0c | 0x0a | 0x09,self.std_out_handle)
+		self.set_cmd_text_color(0x0c | 0x0a | 0x09,self.std_out_handle)
 
 	def log(self,info,erorr,notice=''):
 		sysstr = platform.system()
@@ -193,6 +263,24 @@ class Fake115Client(object):
 		sha.update(r2.content)
 		preid = sha.hexdigest()
 		return preid.upper()
+
+
+	def get_download_url_by_pc(self,pc): # no need auth
+
+		url='http://proapi.115.com/app/chrome/downurl'
+		pc_data=('{"pickcode":"%s"}')%pc		  
+		self.m115_xorinit(Random.new().read(16),4)
+		data='data='+self.m115_encode(pc_data)
+		
+		r = requests.post(url, data=data,headers=self.header)
+		ciphertext=(json.loads(r.content)['data'])
+		plaintext=self.m115_decode(ciphertext)
+
+		jtext=json.loads(plaintext).items()
+		for key, value in jtext:
+			url =value['url']['url']
+
+		return url
 
 	def get_link(self,filename):
 		try:
@@ -283,7 +371,7 @@ class Fake115Client(object):
 				sha1.update(data)
 			totalhash=sha1.hexdigest().upper()
 			ret=self.import_file_with_sha1(blockhash,totalhash,self.get_file_size(filename),os.path.basename(filename))
-	        return ret
+			return ret
 
 	def export_link_to_file(self,outfile,cid): #		
 		uri="http://webapi.115.com/files?aid=1&cid="+str(cid)+"&o=user_ptime&asc=0&offset=0&show_dir=1&limit=5000&code=&scid=&snap=0&natsort=1&source=&format=json"
@@ -342,15 +430,15 @@ class Fake115Client(object):
 
 		req_headers = {'Content-Type': "multipart/form-data; boundary=----7d4a6d158c9"}
 		m = MultipartEncoder(fields=[('name', os.path.basename(filename)), 
-	                             ('key', resp['object']),
-	                             ('policy',resp['policy']),
-	                             ('OSSAccessKeyId', resp['accessid']),
-	                             ('success_action_status', '200'),
-	                             ( 'callback',resp['callback']),
-	                             ('signature',resp['signature']),
-	                             ('file',(os.path.basename(filename),open(filename, 'rb'), 'video/mp4'))],
-	                     		boundary='----7d4a6d158c9'
-	                    )
+								 ('key', resp['object']),
+								 ('policy',resp['policy']),
+								 ('OSSAccessKeyId', resp['accessid']),
+								 ('success_action_status', '200'),
+								 ( 'callback',resp['callback']),
+								 ('signature',resp['signature']),
+								 ('file',(os.path.basename(filename),open(filename, 'rb'), 'video/mp4'))],
+						 		boundary='----7d4a6d158c9'
+						)
 		r = requests.post(resp['host'],headers=req_headers,data=m)
 		try:
 			if json.loads(r.content)['state']==True and json.loads(r.content)['code']==0:
@@ -377,10 +465,10 @@ class Fake115Client(object):
 
 
 def usage():
-    print(
+	print(
 """
 Usage:
--c cid     : Folder cid (default set 0)
+-c cid	 : Folder cid (default set 0)
 -u filename: Upload file form local disk
 -i filename: Import file from hashlink list
 -o filename: Export hashlink list from 115
@@ -419,5 +507,4 @@ if __name__ == '__main__':
 							
 	except getopt.GetoptError:
 		print("Argv error,please input")
-
 
